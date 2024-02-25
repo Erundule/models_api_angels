77be6a7da34d71732db30f0ed5fbe8a33e2134a6
Erundule <camila.claudinoo123@gmail.com>
2024-02-25 15:11:36 -0300

    código para rodar servidor

--- api/routes.py
+++ api/routes.py
@@ -1,1 +1,15 @@
 from api import app
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return "Predict"
+
+@app.route('/parameters', methods=['GET'])
+def parameters():
+    return "Parameters"
+
+          
