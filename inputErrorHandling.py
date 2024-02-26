b174d636d7e1cada82ad640fd9a4d5fe6a509a70
Malbuq <michael-alb@hotmail.com>
2024-02-25 22:30:04 -0300

    Melhorando o codigo do error handling

--- inputErrorHandling.py
+++ inputErrorHandling.py
@@ -7,6 +7,21 @@ def campo_nao_preenchido(campo):
     else:
         return False
 
+def campo_nao_e_inteiro(valor):
+    try:
+        valor = int(valor)
+        return False
+    except ValueError:
+        return True
+
+def data_nao_e_valida(data1, data2):
+    try:
+        data1 = datetime.fromisoformat(data1)
+        data2 = datetime.fromisoformat(data2)
+        return False
+    except ValueError:
+        return True
+
 def checar_peso_anterior_valido(peso_anterior):
     if(campo_nao_preenchido(peso_anterior)):
         return False
@@ -17,108 +32,83 @@ def checar_peso_anterior_valido(peso_anterior):
     except ValueError:
         return False
 
-def checar_risco_gestacional_valido(risco_gestacional):
-    if(campo_nao_preenchido(risco_gestacional)):
+def checar_se_risco_gestacional_valido(risco_gestacional):
+    if(campo_nao_preenchido(risco_gestacional) or campo_nao_e_inteiro(risco_gestacional)):
         return False
-    try:
-        is_risco_gestacional_valido = False
-        risco_gestacional_numerico = int(risco_gestacional)
+    
+    risco_gestacional_numerico = int(risco_gestacional)
 
-        if(risco_gestacional_numerico <= 2 and risco_gestacional_numerico >= 0):
-            is_risco_gestacional_valido = True
+    if(risco_gestacional_numerico >= 0) and risco_gestacional_numerico <= 2:
+        return True
+    
+    return False
 
-        return is_risco_gestacional_valido
-
-    except ValueError:
+def checar_se_escolaridade_valido(escolaridade):
+    if(campo_nao_preenchido(escolaridade) or campo_nao_e_inteiro(escolaridade)):
         return False
-
-def checar_escolaridade_valido(escolaridade):
-    if(campo_nao_preenchido(escolaridade)):
-        return False
-    try:
-        is_escolaridade_valido = False
-        escolaridade_numeric = int(escolaridade)
-
-        if(escolaridade_numeric <= 8 and escolaridade_numeric >= 0):
-            is_escolaridade_valido = True
         
-        return is_escolaridade_valido
+    escolaridade_numeric = int(escolaridade)
 
-    except ValueError:
-        return False
+    if(escolaridade_numeric >= 0 and escolaridade_numeric <= 8):
+        is_escolaridade_valido = True
+    
+    return False
 
 def checar_se_categoria_e_binaria(categoria):
-    if(campo_nao_preenchido(categoria)):
+    if(campo_nao_preenchido(categoria) or campo_nao_e_inteiro(categoria)):
         return False
-    try:
-        is_categoria_binario = False
 
-        categoria_numeric = int(categoria)
+    categoria_numeric = int(categoria)
 
-        if(categoria_numeric == 0 or categoria_numeric == 1):
-            is_categoria_binario = True
+    if(categoria_numeric == 0 or categoria_numeric == 1):
+        return True
 
-        return is_categoria_binario
-
-    except ValueError:
-        return False
+    return False
 
 def checar_se_categoria_e_inteira_positiva(categoria):
-    if(campo_nao_preenchido(categoria)):
+    if(campo_nao_preenchido(categoria) or campo_nao_e_inteiro(categoria)):
         return False
-    try:
-        is_inteira_positiva = False
 
-        categoria_numeric = int(categoria)
+    categoria_numeric = int(categoria)
 
-        if(categoria_numeric >= 0):
-            is_inteira_positiva = True
+    categoria_e_inteiro_positivo = categoria_numeric >= 0
 
-        return is_inteira_positiva
-    
-    except ValueError:
-        return False
+    if(categoria_e_inteiro_positivo):
+        return True
+
+    return False
 
 def idade_e_valida(data_nascimento, data_primeiro_pre_natal): #checa se a idade é valida e se for, retorna a idade
-    if(campo_nao_preenchido(data_nascimento) and campo_nao_preenchido(data_primeiro_pre_natal)):
+    if(campo_nao_preenchido(data_nascimento) or campo_nao_preenchido(data_primeiro_pre_natal) or data_nao_e_valida(data_nascimento, data_primeiro_pre_natal)):
         return False
     
-    try:
-        data_nascimento = datetime.fromisoformat(data_nascimento)
-        data_primeiro_pre_natal = datetime.fromisoformat(data_primeiro_pre_natal)
-    except ValueError:
-        return False
+    data_nascimento = datetime.fromisoformat(data_nascimento)
+    data_primeiro_pre_natal = datetime.fromisoformat(data_primeiro_pre_natal)
 
     idade = abs((int)((data_nascimento - data_primeiro_pre_natal).days/365))
 
     return idade 
 
 def primeiro_pre_natal_valido(data_inicio_gestacao, data_primeiro_pre_natal):#checa se é valido e se for retorna qntd de semanas
-    if(campo_nao_preenchido(data_inicio_gestacao) and campo_nao_preenchido(data_primeiro_pre_natal)):
+    if(campo_nao_preenchido(data_inicio_gestacao) or campo_nao_preenchido(data_primeiro_pre_natal) or data_nao_e_valida(data_inicio_gestacao, data_primeiro_pre_natal)):
         return False
     
-    try:
-        data_inicio_gestacao = datetime.fromisoformat(data_inicio_gestacao)
-        data_primeiro_pre_natal = datetime.fromisoformat(data_primeiro_pre_natal)
-    except ValueError:
-        return False
+    data_inicio_gestacao = datetime.fromisoformat(data_inicio_gestacao)
+    data_primeiro_pre_natal = datetime.fromisoformat(data_primeiro_pre_natal)
 
     quantidadeSemanas = abs((int)((data_inicio_gestacao - data_primeiro_pre_natal).days/7))
 
     return quantidadeSemanas
 
 def semanas_entre_gravidezes_valido(data_inicio_gestacao, data_ultimo_parto):#checa se é valido e se for retorna qntd de semanas
-    if(campo_nao_preenchido(data_inicio_gestacao)):
-        return False
-    
     if(data_ultimo_parto is None):
         return -1
     
-    try:
-        data_inicio_gestacao = datetime.fromisoformat(data_inicio_gestacao)
-        data_ultimo_parto = datetime.fromisoformat(data_ultimo_parto)
-    except ValueError:
+    if(campo_nao_preenchido(data_inicio_gestacao) or  data_nao_e_valida(data_inicio_gestacao, data_ultimo_parto)):
         return False
+    
+    data_inicio_gestacao = datetime.fromisoformat(data_inicio_gestacao)
+    data_ultimo_parto = datetime.fromisoformat(data_ultimo_parto)
 
     quantidadeMeses = abs((int)((data_inicio_gestacao - data_ultimo_parto).days/30))
 
@@ -126,4 +116,3 @@ def semanas_entre_gravidezes_valido(data_inicio_gestacao, data_ultimo_parto):#ch
         quantidadeMeses = 12
     
     return quantidadeMeses
-
