fce804e1f9daed6a03795b1d08df0271997c42e7
Erundule <camila.claudinoo123@gmail.com>
2024-02-26 09:54:45 -0300

    criação do endpoint com parâmetros

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,5 @@
 from api import app
+from flask import request
 
 @app.route('/')
 def index():
@@ -9,7 +10,7 @@ def predict():
     return "Predict"
 
 @app.route('/parameters', methods=['GET'])
-def parameters():
-    return "Parameters"
-
-          
+def model_parameters():
+    parameters = ['previous_weight', 'gestational_risk', 'schooling', 'has_hypertension', 'has_diabetes', 'has_pelvic_surgery','has_urinary_infection', 'has_congenital_malformation', 
+    'has_family_twinship', 'amount_gestation', 'amount_abortion', 'amount_deliveries','amount_cesarean', 'target', 'age', 'first_prenatal', 'time_between_pregnancies']   
+    return parameters
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from api import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+exit()
