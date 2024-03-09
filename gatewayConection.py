e2efa4799c404f7132893ac5d8a2757545d5ce49
Luís Eduardo <luiseduardo0501@protonmail.com>
2024-03-09 19:21:59 -0300

    correção eureka

--- gatewayConection.py
+++ gatewayConection.py
@@ -1,10 +1,10 @@
-from py_eureka_client.eureka_client import EurekaClient
+from py_eureka_client import eureka_client
 
 eureka_server = 'http://localhost:'
 app_name = 'modelo-api-angels'
 instance_host = 'localhost'
 instance_port = 5000
 
-client = EurekaClient(eureka_server=eureka_server, app_name=app_name,
-                      instance_host=instance_host, instance_port=instance_port,
-                      status_page_url="/docs")
+eureka_client.init(eureka_server=eureka_server, app_name=app_name,
+                  instance_host=instance_host, instance_port=instance_port,
+                  status_page_url="/docs")
