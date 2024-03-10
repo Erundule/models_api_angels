3cd68fa97c5fef2f16c4331c1a86745d8bb509cf
elisonmartins <martinselison10@gmail.com>
2024-03-09 21:48:02 -0300

    autenticação das rotas

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from api import app
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+# Configurar a chave secreta para assinar os tokens JWT
+app.config['JWT_SECRET_KEY'] = 'secreto'
+
+jwt = JWTManager(app)
+
+# Registrar o blueprint da API
+from api.routes.routes import api_blueprint
+app.register_blueprint(api_blueprint)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-    
